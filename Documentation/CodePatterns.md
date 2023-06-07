fffe10f6248d55d4d90a81ddda97a4ddcb47f5eb
Timo Pedersen <timo.pedersen@beijerelectronics.com>
2023-06-07 12:47:33 +0200

    Working on pattern 3, added converter

---
+++ Documentation/CodePatterns.md
@@ -0,0 +1,17 @@
+# Code patterns
+
+| No | Source | Target | Comment |
+| --- | --- | --- | --- |
+| 1 | Using Rhino.Mocks; | using NSubstitute; | |
+| 3 | MockRepository.GenerateStub<T>(); | Substitute.For<T>() |  |
+|  |  |  |  |
+|  |  |  |  |
+|  |  |  |  |
+|  |  |  |  |
+|  |  |  |  |
+|  |  |  |  |
+|  |  |  |  |
+|  |  |  |  |
+|  |  |  |  |
+|  |  |  |  |
+|  |  |  |  |
---
+++ WpfAnalyserGUI/Converters/BoolToVisibilityConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Converters;
+
+[ValueConversion(typeof(bool), typeof(Visibility))]
+public sealed class BoolToVisibilityConverter : IValueConverter
+{
+    public Visibility TrueValue { get; set; }
+    public Visibility FalseValue { get; set; }
+
+    public BoolToVisibilityConverter()
+    {
+        TrueValue = Visibility.Visible;
+        FalseValue = Visibility.Collapsed;
+    }
+
+    public object Convert(object? value, Type targetType, 
+        object? parameter, CultureInfo culture)
+    {
+        if (value is bool b)
+            return b ? TrueValue : FalseValue;
+
+        return FalseValue;
+    }
+
+    public object ConvertBack(object? value, Type targetType, 
+        object? parameter, CultureInfo culture)
+    {
+        if (Equals(value, TrueValue))
+            return true;
+        if (Equals(value, FalseValue))
+            return false;
+
+        return false;
+    }
+}
